c4e9704b69c395c8e30bbc2e2d4ae5f190e29a63
Alexandra Millatmal <alexandrajmh@gmail.com>
2015-02-12 22:21:02 -0600

    Rearranged files!

--- main.rb
+++ main.rb
@@ -5,11 +5,11 @@ require 'sqlite3'
 
 DATABASE = SQLite3::Database.new("warehouse_mgr.db")
 
-require_relative "warehouse_mgr_setup.rb"
-require_relative "warehouse_methods.rb"
-require_relative "location.rb"
-require_relative "category.rb"
-require_relative "product.rb"
+require_relative "models/warehouse_mgr_setup.rb"
+require_relative "models/warehouse_methods.rb"
+require_relative "models/location.rb"
+require_relative "models/category.rb"
+require_relative "models/product.rb"
 
 
 
@@ -143,6 +143,3 @@ get "/added_product" do
   
   erb :"product/added_product"
 end
-
-# get "/edit" do
-# end
